b4a13f73ce9a19042e876cd9ac6f322553ab7de0
nutqp <s6070095@kmitl.ac.th>
2014-11-28 00:12:11 +0700

    check_ERROR

--- gui.py
+++ gui.py
@@ -2,11 +2,21 @@
 from Tkinter import *
 from PIL import Image, ImageTk
 import webbrowser
+import tkMessageBox
 def google_map(event):
+    ''' Connect to GoogleMap '''
     webbrowser.open_new("https://www.google.co.th/maps/dir///@13.7245995,100.6331106,11z")
-    
+def check_error():
+    ''' Check the input '''
+    try:
+        distanc1 = int(distanc.get())
+        time1 = int(time.get())
+    except ValueError:
+        tkMessageBox.showerror("Error input", "No valid integer! Please try again ...")
 def page_root():
     '''  First page, 2 variables input '''
+    global distanc
+    global time
     root = Tk()
     root.geometry('760x585+280+70')
     root.title("Taxi meter")
@@ -20,7 +30,7 @@ def page_root():
     BG_click_label.place(x=274, y=297)
     BG_click_label.bind("<Button-1>", google_map)
     BG_calculate = ImageTk.PhotoImage(Image.open("calculate.gif"))
-    BG_calculate_label = Button(root, image=BG_calculate, cursor="hand2").place(x=325, y=425)
+    BG_calculate_label = Button(root, image=BG_calculate, command=check_error, cursor="hand2").place(x=325,y=425)
     distanc = IntVar()
     time = IntVar()
     button_distance = Entry(root, textvariable=distanc).place(x=282, y=260)
