bd40bb2a710e278e74160fede7ba418f6dbd37ac
nutqp <s6070095@kmitl.ac.th>
2014-11-27 17:55:12 +0700

    import webbrowser

---
+++ gui.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+from PIL import Image, ImageTk
+import webbrowser
+def google_map(event):
+    webbrowser.open_new("https://www.google.co.th/maps/dir///@13.7245995,100.6331106,11z")
+    
+def page_root():
+    '''  First page, 2 variables input '''
+    root = Tk()
+    root.geometry('760x585+280+70')
+    root.title("Taxi meter")
+    frame = Frame(root).pack()
+    canvas = Canvas(frame, bg="white", width=750, height=580)
+    canvas.pack()
+    photoimage = ImageTk.PhotoImage(file="main_taxi.gif")
+    canvas.create_image(365, 289, image=photoimage)
+    BG_click = ImageTk.PhotoImage(Image.open("click.gif"))
+    BG_click_label = Button(root, image=BG_click, cursor="hand2")
+    BG_click_label.place(x=274, y=297)
+    BG_click_label.bind("<Button-1>", google_map)
+    BG_calculate = ImageTk.PhotoImage(Image.open("calculate.gif"))
+    BG_calculate_label = Button(root, image=BG_calculate, cursor="hand2").place(x=325, y=425)
+    distanc = IntVar()
+    time = IntVar()
+    button_distance = Entry(root, textvariable=distanc).place(x=282, y=260)
+    button_time = Entry(root, textvariable=time).place(x=282, y=380)
+    mainloop()
+page_root()
