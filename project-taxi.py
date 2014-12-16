56ce7334bbc1ed0b9593aad8594cad3c3d896d88
nutqp <s6070095@kmitl.ac.th>
2014-12-16 22:41:49 +0700

    program taxi-meter

--- project-taxi.py
+++ project-taxi.py
@@ -44,32 +44,28 @@ def show_detail():
     else:
         price = 534 + (kilometre-80)*8.5
     price = int(price) #price
-    minute1 = (int(floor((minute*1.50)/2)*2))+1 #minute
+    minute1 = (int(floor((minute*1.50)/2)*2))-1 #minute
     total = price + minute1 #total of price + minute
     ''' open page 2 '''
     global page_two
     page_two = Tk()
     page_two.title("Taxi meter")
     page_two.geometry('760x585+280+70')
-    
     frame = Frame(page_two).pack()
     canvas = Canvas(frame, bg="white", width=750, height=580)
     canvas.pack()
     photoimage = ImageTk.PhotoImage(file="main_output.gif")
     canvas.create_image(365, 289, image=photoimage)
-
+    ''' show output '''
+    Label(page_two,text = kilometre, fg = "red", font = "Times 15 bold").place(x=525, y=190)
+    Label(page_two,text = minute, fg = "red", font = "Times 15 bold").place(x=525, y=228)
+    Label(page_two,text = price, fg = "red", font = "Times 15 bold").place(x=525, y=265)
+    Label(page_two,text = minute1, fg = "red", font = "Times 15 bold").place(x=525, y=305)
+    Label(page_two,text = total, fg = "red", font = "Times 30 bold").place(x=484, y=352)
     BG_home = ImageTk.PhotoImage(Image.open("home.gif"))
     BG_click_home = Button(page_two, image=BG_home, cursor="hand2",command=back_page_root).place(x=55,y=420)
-
     BG_exit = ImageTk.PhotoImage(Image.open("exit.gif"))
     BG_click_exit = Button(page_two, image=BG_exit, cursor="hand2", command=quit_pagetwo).place(x=147,y=420)
-    
-    ''' show output '''
-    Label(page_two,text = kilometre, fg = "red", font = "Times 15 bold").place(x=525,y=190)
-    Label(page_two,text = minute, fg = "red", font = "Times 15 bold").place(x=525,y=228)
-    Label(page_two,text = price, fg = "red", font = "Times 15 bold").place(x=525,y=265)
-    Label(page_two,text = minute1, fg = "red", font = "Times 15 bold").place(x=525,y=305)
-    Label(page_two,text = total, fg = "red", font = "Times 30 bold").place(x=484,y=352)
     mainloop()
 def page_root():
     ''' First page, 2 variables input '''
@@ -84,14 +80,12 @@ def page_root():
     canvas.pack()
     photoimage = ImageTk.PhotoImage(file="main_taxi.gif")
     canvas.create_image(365, 289, image=photoimage)
-    
     BG_click = ImageTk.PhotoImage(Image.open("click.gif"))
     BG_click_label = Button(root, image=BG_click, cursor="hand2")
     BG_click_label.place(x=274, y=297)
     BG_click_label.bind("<Button-1>", google_map)
     BG_calculate = ImageTk.PhotoImage(Image.open("calculate.gif"))
-    BG_calculate_label = Button(root, image=BG_calculate, command=check_error, cursor="hand2").place(x=325,y=425)
-    
+    BG_calculate_label = Button(root, image=BG_calculate, command=check_error, cursor="hand2").place(x=325, y=425)
     distanc = StringVar()
     time = StringVar()
     button_distance = Entry(root, textvariable=distanc).place(x=282, y=260)
